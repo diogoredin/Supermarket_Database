d76e14ace55501d20db859a419491e62a0ba2e1e
Diogo Redin <diogo.redin@gmail.com>
2017-11-28 19:39:31 +0000

    Adding my first Query (b), Fixing the Schema and Database Visualizer

--- queries.sql
+++ queries.sql
@@ -1,11 +1,20 @@
+----------------------------------------
+-- Queries
+----------------------------------------
+
+-- b)
+SELECT f.nome, f.nif
+FROM fornecedor f, produto p, categoria_simples c
+WHERE p.categoria = c.nome and p.fornecedor = f.nif;
+
 -- d)
 SELECT ean
 FROM fornece_sec
 GROUP BY ean
-HAVING count(nif) > 10
+HAVING count(nif) > 10;
 
 -- e)
 SELECT ean
 FROM reposicao
 GROUP BY ean
-HAVING count(operador) = 1
+HAVING count(operador) = 1;
--- schema.sql
+++ schema.sql
@@ -15,11 +15,6 @@ drop table categoria;
 -- Criacao das Tabelas
 ----------------------------------------
 
--- As restricoes de nome sao globais na base de dados.
--- Portanto, vamos utilizar as seguintes regras para nao as confundir:
---   1. pk_table para os nomes de chaves primarias
---   2. fk_tabela_outra para os nomes de chaves estrangeiras
-
 -- Categoria
 create table categoria (
     nome varchar(15) not null unique,
@@ -42,8 +37,9 @@ create table super_categoria (
 
 -- Constituida
 create table constituida (
-    super_categoria varchar(15),
-    categoria varchar(15),
+    super_categoria varchar(15) not null,
+    categoria varchar(15) not null,
+	unique (super_categoria, categoria),
     constraint categoria_ciclica check (super_categoria != categoria),
     constraint fk_super_categoria foreign key (super_categoria) references super_categoria(nome),
     constraint fk_categoria foreign key (categoria) references categoria(nome),
@@ -53,17 +49,17 @@ create table constituida (
 -- Fornecedor
 create table fornecedor (
     nif numeric(9,0) not null unique,
-    nome varchar(15),
+    nome varchar(15) not null unique,
     constraint pk_fornecedor primary key (nif)
 );
 
 -- Produto
 create table produto (
     ean numeric(13,0) not null unique,
-    design varchar(50),
-    categoria varchar(15),
-    fornecedor numeric(9,0),
-    instante timestamp,
+    design varchar(50) not null,
+    categoria varchar(15) not null,
+    fornecedor numeric(9,0) not null,
+    instante timestamp not null,
     constraint fk_categoria foreign key (categoria) references categoria(nome),
     constraint fk_fornecedor foreign key (fornecedor) references fornecedor(nif),
     constraint pk_produto primary key (ean)
@@ -71,8 +67,8 @@ create table produto (
 
 -- Fornecedor Secundario
 create table fornece_sec (
-    nif numeric(9,0),
-    ean numeric(13,0),
+    nif numeric(9,0) not null unique,
+    ean numeric(13,0) not null,
     constraint fk_fornecedor foreign key (nif) references fornecedor(nif),
     constraint fk_produto foreign key (ean) references produto(ean),
     constraint pk_fornece_sec primary key (nif, ean)
@@ -81,15 +77,15 @@ create table fornece_sec (
 -- Corredor
 create table corredor (
     nro numeric(4,0) not null unique,
-    largura numeric(3,0),
+    largura numeric(3,0) not null,
     constraint pk_corredor primary key (nro)
 );
 
 -- Prateleira
 create table prateleira (
-    nro numeric(4,0),
-    lado varchar(1),
-    altura numeric(3,0),
+    nro numeric(4,0) not null,
+    lado varchar(1) not null,
+    altura numeric(3,0) not null,
     unique (nro, lado, altura),
     constraint fk_corredor foreign key (nro) references corredor(nro),
     constraint pk_prateleira primary key (nro)
@@ -97,13 +93,13 @@ create table prateleira (
 
 -- Planograma
 create table planograma (
-    ean numeric(13,0),
-    nro numeric(4,0),
-    lado varchar(1),
-    altura numeric(3,0),
-    face varchar(1),
-    unidades numeric(4,0),
-    loc varchar(15),
+    ean numeric(13,0) not null unique,
+    nro numeric(4,0) not null,
+    lado varchar(1) not null,
+    altura numeric(3,0) not null,
+    face varchar(1) not null,
+    unidades numeric(4,0) not null,
+    loc varchar(15) not null,
     constraint fk_produto foreign key (ean) references produto(ean),
     constraint fk_prateleira foreign key (nro, lado, altura) references prateleira(nro, lado, altura),
     constraint pk_planograma primary key (ean, nro, lado, altura)
@@ -111,20 +107,22 @@ create table planograma (
 
 -- Evento Reposicao
 create table evento_reposicao (
-    operador varchar(15),
-    instante timestamp not null unique,
+    operador varchar(15) not null,
+    instante timestamp not null,
+	unique (operador, instante),
     constraint pk_evento_reposicao primary key (operador, instante)
 );
 
 -- Reposicao
 create table reposicao (
-    ean numeric(13,0),
-    nro numeric(4,0),
-    lado char(1),
-    altura numeric(3,0),
-    operador varchar(15),
-    instante timestamp,
-    unidades numeric(4,0),
+    ean numeric(13,0) not null,
+    nro numeric(4,0) not null,
+    lado char(1) not null,
+    altura numeric(3,0) not null,
+    operador varchar(15) not null,
+    instante timestamp not null,
+    unidades numeric(4,0) not null,
+	unique (ean, nro, lado, altura, operador, instante, unidades),
     constraint fk_planograma foreign key (ean, nro, lado, altura) references planograma(ean, nro, lado, altura),
     constraint fk_evento_reposicao foreign key (operador, instante) references evento_reposicao(operador, instante),
     constraint pk_reposicao primary key (ean, nro, lado, altura, operador, instante)
