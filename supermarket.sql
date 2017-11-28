54ee8bb32f57985ddb7c957fab2a387bbf152aca
Diogo Redin <diogo.redin@gmail.com>
2017-11-28 12:00:42 +0000

    Populated Database

--- supermarket.sql
+++ supermarket.sql
@@ -22,28 +22,28 @@ drop table categoria;
 
 -- Categoria
 create table categoria (
-    nome char(15) not null unique,
+    nome varchar(15) not null unique,
     constraint pk_categoria primary key (nome)
 );
 
 -- Categoria Simples
 create table categoria_simples (
-    nome char(15) not null unique,
+    nome varchar(15) not null unique,
     constraint fk_categoria foreign key (nome) references categoria(nome),
     constraint pk_categoria_simples primary key (nome)
 );
 
 -- Super Categoria
 create table super_categoria (
-    nome char(15) not null unique,
+    nome varchar(15) not null unique,
     constraint fk_categoria foreign key (nome) references categoria(nome),
     constraint pk_super_categoria primary key (nome)
 );
 
 -- Constituida
 create table constituida (
-    super_categoria char(15),
-    categoria char(15),
+    super_categoria varchar(15),
+    categoria varchar(15),
     constraint categoria_ciclica check (super_categoria != categoria),
     constraint fk_super_categoria foreign key (super_categoria) references super_categoria(nome),
     constraint fk_categoria foreign key (categoria) references categoria(nome),
@@ -53,15 +53,15 @@ create table constituida (
 -- Fornecedor Primario
 create table fornecedor (
     nif numeric(9,0) not null unique,
-    nome numeric(15,0),
+    nome varchar(15),
     constraint pk_fornecedor primary key (nif)
 );
 
 -- Produto
 create table produto (
     ean numeric(13,0) not null unique,
-    design char(50),
-    categoria char(15),
+    design varchar(50),
+    categoria varchar(15),
     fornecedor numeric(9,0),
     instante timestamp,
     constraint fk_categoria foreign key (categoria) references categoria(nome),
@@ -88,7 +88,7 @@ create table corredor (
 -- Prateleira
 create table prateleira (
     nro numeric(4,0),
-    lado char(1),
+    lado varchar(1),
     altura numeric(3,0),
     unique (nro, lado, altura),
     constraint fk_corredor foreign key (nro) references corredor(nro),
@@ -99,11 +99,11 @@ create table prateleira (
 create table planograma (
     ean numeric(13,0),
     nro numeric(4,0),
-    lado char(1),
+    lado varchar(1),
     altura numeric(3,0),
-    face char(1),
+    face varchar(1),
     unidades numeric(4,0),
-    loc char(15),
+    loc varchar(15),
     constraint fk_produto foreign key (ean) references produto(ean),
     constraint fk_prateleira foreign key (nro, lado, altura) references prateleira(nro, lado, altura),
     constraint pk_planograma primary key (ean, nro, lado, altura)
@@ -111,7 +111,7 @@ create table planograma (
 
 -- Evento Reposicao
 create table evento_reposicao (
-    operador char(15),
+    operador varchar(15),
     instante timestamp not null unique,
     constraint pk_evento_reposicao primary key (operador, instante)
 );
@@ -122,7 +122,7 @@ create table reposicao (
     nro numeric(4,0),
     lado char(1),
     altura numeric(3,0),
-    operador char(15),
+    operador varchar(15),
     instante timestamp,
     unidades numeric(4,0),
     constraint fk_planograma foreign key (ean, nro, lado, altura) references planograma(ean, nro, lado, altura),
@@ -134,25 +134,26 @@ create table reposicao (
 -- Populate Relations
 ----------------------------------------
 
+insert into categoria values ('Frescos');
+insert into super_categoria values ('Frescos');
+
+insert into categoria values ('Vegetais');
+insert into categoria_simples values ('Vegetais');
+
 insert into categoria values ('Carne');
 insert into categoria_simples values ('Carne');
 
 insert into categoria values ('Peixe');
 insert into categoria_simples values ('Peixe');
 
-insert into categoria values ('Vegetais');
-insert into categoria_simples values ('Vegetais');
-
-insert into super_categoria values ('Frescos');
-
-insert into constituida values ('Frescos', 'Peixe');
-insert into constituida values ('Frescos', 'Carne');
 insert into constituida values ('Frescos', 'Vegetais');
+insert into constituida values ('Frescos', 'Carne');
+insert into constituida values ('Frescos', 'Peixe');
 
-insert into fornecedor values (987654321, 'Makro');
 insert into fornecedor values (123456789, 'Continente');
+insert into fornecedor values (987654321, 'Makro');
 
-insert into produto values (1234567891234, 'Salmao', 'Peixe', 'Makro', now());
+insert into produto values (1234567891234, 'Salmao', 'Peixe', 987654321, '2017-11-28 11:58:48');
 
 insert into fornece_sec values (123456789, 1234567891234);
 
@@ -161,5 +162,5 @@ insert into prateleira values (1, 'e', 10);
 
 insert into planograma values (1234567891234, 1, 'e', 10, 'f', 4, 'Frigorificos');
 
-insert into evento_reposicao values ('Andre', now());
-insert into reposicao values (1234567891234, 1, 'e', 10, 'f', 4, 'Frigorificos', 'Andre', now());
+insert into evento_reposicao values ('Andre', '2017-11-28 11:58:48');
+insert into reposicao values (1234567891234, 1, 'e', 10, 'Andre', '2017-11-28 11:58:48', 4);
